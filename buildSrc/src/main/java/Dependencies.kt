c65d2ee0a53bfddf867a7eb16b4bfb668bcb50f5
okanaydin <okan.aydin@isik.edu.tr>
2020-05-08 05:17:02 +0300

    add Kotlin DSL for Gradle

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,17 @@
+object Versions {
+    const val appCompatVersion = "1.1.0"
+    const val coreKtxVersion = "1.2.0"
+    const val constraintlayoutVersion = "1.1.3"
+    const val junitVersion = "4.12"
+    const val testJunitVersion = "1.1.1"
+    const val espressoCoreVersion = "3.2.0"
+}
+
+object Dependencies {
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
+    const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtxVersion}"
+    const val constraintlayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintlayoutVersion}"
+    const val junit = "junit:junit:${Versions.junitVersion}"
+    const val testJunit = "androidx.test.ext:junit:${Versions.testJunitVersion}"
+    const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espressoCoreVersion}"
+}
